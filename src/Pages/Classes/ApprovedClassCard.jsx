81280d527d3e9a625c8d252df364e7544d584c02
Shakhawat <syedarafatcse@gmail.com>
2023-06-08 11:56:55 +0600

    class select to student selected class is implemented

--- src/Pages/Classes/ApprovedClassCard.jsx
+++ src/Pages/Classes/ApprovedClassCard.jsx
@@ -1,13 +1,57 @@
+import Swal from "sweetalert2";
+import useAuth from "../../Hooks/useAuth";
+import { useLocation, useNavigate } from "react-router-dom";
 
 
 const ApprovedClassCard = ({ item }) => {
+   const location = useLocation();
+   const from = location.state?.from?.pathname || '/';
+   const navigate = useNavigate();
+
    const { _id, className, classImage, instructorName, instructorImage, instructorEmail, availableSeats, enrolled, price } = item;
 
    const isAdmin = false;
    const isInstructor = false;
 
-   const handleSelectClass = () => {
+   const { user } = useAuth()
 
+   const handleSelectClass = () => {
+      // console.log(user.email);
+      const selectClass = { studentEmail: user?.email, classId: _id, className, classImage, price, instructorName, instructorImage, instructorEmail, availableSeats, enrolled }
+
+      if (user && user.email) {
+         fetch('http://localhost:5000/selectedClass', {
+            method: 'POST',
+            headers: { "content-type": "application/json" },
+            body: JSON.stringify(selectClass)
+
+         })
+            .then(res => res.json())
+            .then(data => {
+               console.log(data)
+               if (data.insertedId) {
+                  Swal.fire({ position: 'center', icon: 'success', title: 'Class is Selected!!', showConfirmButton: false, timer: 1500 });
+               }
+            })
+      }
+
+      else {
+         Swal.fire({
+            title: 'You are not Login!',
+            text: "You have to login firest to select any class!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, Login!'
+         }).then((result) => {
+            if (result.isConfirmed) {
+
+               navigate('/login', { state: { from: location } })
+            }
+         })
+
+      }
    }
 
    // console.log(item)
